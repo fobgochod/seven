cda211a9d1b3b3a7039eddccfc5843b18e02dd1e
zhouxiao <764800230@qq.com>
2020-07-07 01:02:21 +0800

    no message

--- .vuepress/config/sidebar.js
+++ .vuepress/config/sidebar.js
@@ -5,6 +5,26 @@ module.exports = [
         path: '/SUMMARY.md'
     },
     {
+        title: '前言',
+        collapsable: false,
+        children: [
+            '/introduction/about-me',
+            '/introduction/about-the-project',
+            '/introduction/guide/quick-start'
+        ]
+    },
+    {
+        title: '7月8月',
+        collapsable: false,
+        path: '/mashibing/',
+        children: []
+    },
+    {
+        title: 'DSweet',
+        collapsable: false,
+        children: []
+    },
+    {
         title: '附录',
         collapsable: false,
         children: [
---
+++ introduction/about-the-project.md
@@ -0,0 +1,34 @@
+# 记记笔记
+
+- 记记笔记
+- 记记生活
+- 少年，别太懒
+
+# 养养猫
+
+## 巧克力
+
+- 暹罗 蓝色 母猫
+- 2016年08月01日
+
+<center>
+<img src='./images/chocolate.png' ></img>
+</center>
+
+## 小丸子
+
+- 布偶 蓝双 公猫
+- 2018年06月01日
+
+<center>
+<img src='./images/ball.png' ></img>
+</center>
+
+## 萝卜
+
+- 布偶 海豹 公猫
+- 2019年08月22日
+
+<center>
+<img src='./images/robo.png' ></img>
+</center>
